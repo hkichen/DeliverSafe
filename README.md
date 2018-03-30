adbaeb1afd21851b8ee1d8273da289b9fb5a0996
hkichen <hkichen@gmail.com>
2018-03-29 23:09:29 -0400

    trying new geocodin with google maps js api sample

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # DeliverSafe
 A mobile friendly app that allows delivery drivers to check crime in their current area
 
-Google Maps API Keys
+#Google Maps API Keys
 **Web browser API Key: AIzaSyAiYx21r9sMd6jLLfHnMyuYFb9VT_cDcZc
 **iOS API Key: AIzaSyASbpgmxEB7-BkUG6l-LgTrS-Xv9i3jK34
 **Android API Key: AIzaSyDiRPxEWtsXYpgn2fOU3B68ZWWMAWLDIiE
+
+#JQuery CDN
+for peeps (like me) who keep forgetting to add this script ot their html
+jquery cdn:
+<script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+
+
+API key for geocoding: AIzaSyDhyViOJ8VEeLmwuCuyzmXtrw-S6PX4bNY
+url: https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
 
 
--- assets/app.js
+++ assets/app.js
@@ -1,45 +1,33 @@
-//make initial map show up on the page
-var map;
-
-$("#getMap").click(function initMap(){
-    map = new google.maps.Map(document.getElementById('map'), {
-		center: {
-			lat: -34.397,
-			lng: 150.644
-		},
-		zoom: 15
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: {lat: -34.397, lng: 150.644}
     });
-});
-
-//grabb locations via array locations inside an array---CONVERT TO Array of objects!
-var locations = [
-    ['Assault', -33.890542, 151.274856, 4],
-    ['Armed Robbery', -33.923036, 151.259052, 5],
-    ['Breaking and Entering', -34.028249, 151.157507, 3],
-    ['Arson', -33.80010128657071, 151.28747820854187, 2],
-    ['Homicide', -33.950198, 151.259302, 1]
-];
-
-var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: new google.maps.LatLng(-33.92, 151.25),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-});
-
-var infowindow = new google.maps.InfoWindow();
-
-var marker, i;
-
-for (i = 0; i < locations.length; i++) {  
-    marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-    });
+    var geocoder = new google.maps.Geocoder();
     
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-            infowindow.setContent(locations[i][0]);
-            infowindow.open(map, marker);
+    $('#submit').on('click', function() {
+        geocodeAddress(geocoder, map);
+    });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
         }
-    })(marker, i));
+    });
+}
+
+
+function resetMap() {
+    $('#reset').on("click", function() {
+        initMap();  
+    })
 }
