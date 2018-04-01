2bd194f6bf6abe9035c630276d5160040eac0841
hkichen <hkichen@gmail.com>
2018-04-01 19:16:43 -0400

    got the app to grab your location

--- assets/app2.js
+++ assets/app2.js
@@ -1,5 +1,4 @@
 //Google Map call and functionality
-
 function initMap() {
     var newCenter = {lat: 33.761349, lng:-84.389437}
 	var map = new google.maps.Map(document.getElementById('map'), {
@@ -7,9 +6,27 @@ function initMap() {
 		center: newCenter,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
     });
+    
+    var geocoder = new google.maps.Geocoder();
+    
+    document.getElementById('submit').addEventListener('click', function() {
+        geocodeAddress(geocoder, map);
+    });
 }
 
-$('#submit').click(function() {
-    var yourCenter = $('#addressInput').val();
-    console.log(yourCenter);
-})
+//grab user input of address and then geocodes it
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+            console.log(marker.position.latLng);
+        } else {
+            console.log('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
