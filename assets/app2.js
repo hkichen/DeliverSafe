634156be4190591c88929bb92fc1c5425421427a
hkichen <hkichen@gmail.com>
2018-04-01 16:20:24 -0400

    working with getting information to show up on markers dropped on map

--- assets/app2.js
+++ assets/app2.js
@@ -1,30 +1,48 @@
-var map;
-
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: {
-            lat: 33.749, 
-            lng: -84.388
-        }
-    });
+    var locations = [
+        {
+            name: "Fountain of Rings",
+            address: "265 Park Ave W NW",
+            lat: 33.759491,
+            lng: -84.392896
+        },
+        {
+            name: "Hard Rock Cafe",
+            address: "215 Peachtree St NE",
+            lat: 33.759865,
+            lng: -84.387381
+        },
+        {
+            name: "STATS Brewpub",
+            address: "300 Marietta St NW",
+            lat: 33.761453,
+            lng: -84.396307
+        },
+    ];
+    
+    //var newCenter = $("#addressInput").val();
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: new google.maps.LatLng(33.761349, -84.389437),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+    
+	var infowindow = new google.maps.InfoWindow({});
+	var marker, i;
+    
+	for (i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i], locations[i]),
+			map: map
+        });
+
+		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+			return function () {
+				infowindow.setContent(locations[i]);
+				infowindow.open(map, marker);
+			}
+		})(marker, i));
+    }
+    console.log(locations);
 }
-
-var broadway = {
-    info: '<strong>Chipotle on Broadway</strong><br>\
-                5224 N Broadway St<br> Chicago, IL 60640',
-    lat: 41.976816,
-    long: -87.659916
-};
-
-var locations = [
-    [broadway.info, broadway.lat, broadway.long, 0],
-    [belmont.info, belmont.lat, belmont.long, 1],
-    [sheridan.info, sheridan.lat, sheridan.long, 2],
-];
-
-var map = new google.maps.Maps(document.getElementById("map"), {
-    zoom: 15,
-    center: new google.maps.Maps.LatLng(41.976816, -87.659916),
-});
-
