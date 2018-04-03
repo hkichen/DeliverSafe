fcf756df63512b807b0d9aa8a48e072668273c15
Carrie <thelongdinosaur@gmail.com>
2018-04-03 18:18:30 -0400

    second attempt to push

--- assets/app.js
+++ assets/app.js
@@ -1,71 +1,78 @@
-//global variable to store user's address to be used in calling crime data api
-//Google Map call and functionality
-function initMap() {
-    var newCenter = {lat: 33.761349, lng:-84.389437}
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15,
-		center: newCenter,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+
+// should grab latitude and longitute from search results as the criteria for search of crime api
+
+// search for crimes in that area 
+
+// return first 10 most recent crimes in destination area
+
+// should provide offenses [sex crimes, robbery, assault, homicide], locations of offenses, as well as date and if available, name of offender and add them to table as new rows
+
+// "https://api.spotcrime.com/crimes.json?lat= " + [lat] + "&lon= " + [lon] "&radius=0.02&key=heythisisforpublicspotcrime.comuse-forcommercial-or-research-use-call-877.410.1607-or-email-pyrrhus-at-spotcrime.com"
+
+
+// VARIABLES ===================================================================================
+
+// var destCity = [should be pulled from search results... latitude and longitude of destination?]
+// var destLat = [should be pulled from search results... latitude and longitude of destination?]
+// var destLon = [should be pulled from search results... latitude and longitude of destination?]
+// .crimes(type, date, address, lat, lon)
+// var lat = 
+// var lon = 
+
+// =============================================================================================
+
+
+// POSSIBLE API CALLS ==========================================================================
+
+// crimeSearch = "https://api.spotcrime.com/crimes.json?lat=33.7837971&lon=-84.39996830000001&radius=0.02&key=heythisisforpublicspotcrime.comuse-forcommercial-or-research-use-call-877.410.1607-or-email-pyrrhus-at-spotcrime.com"
+
+// response
+    //"crimes" object array includes "type":, "date":, "address":, "lat":, and "lon":
+
+// =============================================================================================
+
+
+// FUNCTIONS ===================================================================================
+
+$(document).ready(function() {
+
+    // need function to register when "Search" button is clicked
+    // grab lat and long from search response
+    // need function to send API call which grabs the lat and long from the response 
+    // then add/concatenates this as criteria in API call
+    // then call function
+    // add response to table and mark on the map 
     
-    var geocoder = new google.maps.Geocoder();
-    
-    document.getElementById('submit').addEventListener('click', function() {
-        geocodeAddress(geocoder, map);
-    });
+    var crimeSearch = function(searchSpotCrime) {
+        // could not get this to work, plug in once testing to table has been successful
+        //try again to obtain key from spotcrime?
+        // var queryURL = `https://api.spotcrime.com/crimes.json?lat="${33.761349}"&lon="${-84.389437}"&radius=0.02&key=heythisisforpublicspotcrime.comuse-forcommercial-or-research-use-call-877.410.1607-or-email-pyrrhus-at-spotcrime.com`
+        
+        var queryURL = 'https://api.spotcrime.com/crimes.json?lat=33.7837971&lon=-84.39996830000001&radius=0.02&key=heythisisforpublicspotcrime.comuse-forcommercial-or-research-use-call-877.410.1607-or-email-pyrrhus-at-spotcrime.com'
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        // store the retrieved data inside of an object called "response"
+          }).then(function(response) {
+        // check the query
+            console.log(queryURL);
+        // check the response
+            console.log(response);
+        // create a new row
+            createRow(response);
+        crimesearch()
+
+        // add data to table body
+        var createRow = function(data) {
+            var tBody = $("tbody");
+            var tRow = $("<tr>");
+
+            $("#crimeTableHead").append(
+                "<tr><td>" + response.crimes("type", "date", "address") + "</td></tr>"
+            )
+
+        });
+    }
 }
 
-//grab user input of address and then geocodes it, puts a marker on the map
-var yourAddress;
-function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('address').value;
-    geocoder.geocode({'address': address}, function(results, status) {
-        if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: resultsMap,
-                position: results[0].geometry.location
-            });
-            console.log('//global variable to store user's address to be used in calling crime data api
-            //Google Map call and functionality
-            function initMap() {
-                var newCenter = {lat: 33.761349, lng:-84.389437}
-                var map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 15,
-                    center: newCenter,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                });
-                
-                var geocoder = new google.maps.Geocoder();
-                
-                document.getElementById('submit').addEventListener('click', function() {
-                    geocodeAddress(geocoder, map);
-                });
-            }
-            
-            //grab user input of address and then geocodes it, puts a marker on the map
-            var yourAddress;
-            function geocodeAddress(geocoder, resultsMap) {
-                var address = document.getElementById('address').value;
-                geocoder.geocode({'address': address}, function(results, status) {
-                    if (status === 'OK') {
-                        resultsMap.setCenter(results[0].geometry.location);
-                        var marker = new google.maps.Marker({
-                            map: resultsMap,
-                            position: results[0].geometry.location
-                        });
-                        console.log('lat: ' + marker.position.lat() + ', ' + 'lng: ' + marker.position.lng());
-                        //console.log(marker.position.lng())
-                    } else {
-                        console.log('Geocode was not successful for the following reason: ' + status);
-                    }
-                    yourAddress = {lat: (marker.position.lat()), lng: (marker.position.lng())}
-                });
-            }: ' + marker.position.lat() + ', ' + 'lng: ' + marker.position.lng());
-            //console.log(marker.position.lng())
-        } else {
-            console.log('Geocode was not successful for the following reason: ' + status);
-        }
-        yourAddress = {lat: (marker.position.lat()), lng: (marker.position.lng())}
-    });
-}
+// ===============================================================================================
