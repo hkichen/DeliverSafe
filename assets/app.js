2b03fe12412d3fcc9efae80496f353ec428b9818
Carrie <thelongdinosaur@gmail.com>
2018-04-03 20:56:58 -0400

    adjusted table append so that it skips first column

--- assets/app.js
+++ assets/app.js
@@ -1,40 +1,39 @@
+// added this to be able to push
 
-        // added this to be able to push
-        
-        //This call only works if your browser has the CORS plugin!!
-        //ajax call to crime data using a lat and lng
-        var place = {
-            lat: 33.775618,
-            lng: -84.396285
+//This call only works if your browser has the CORS plugin!!
+//ajax call to crime data using a lat and lng
+var place = {
+    lat: 33.775618,
+    lng: -84.396285
+}
+
+// https://api.spotcrime.com/crimes.json?lat=" + place.lat + "&lon=" + place.lng + "&radius=0.02&key=heythisisforpublicspotcrime.comuse-forcommercial-or-research-use-call-877.410.1607-or-email-pyrrhus-at-spotcrime.com
+var queryURL= "https://api.spotcrime.com/crimes.json?lat=33.775618&lon=-84.396285&radius=0.02&key=heythisisforpublicspotcrime.comuse-forcommercial-or-research-use-call-877.410.1607-or-email-pyrrhus-at-spotcrime.com";
+
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+}).then(function(response) {
+    console.log(response.crimes[0]);
+    var crimeLocation = [
+        {
+            lat: response.crimes.lat,
+            lng: response.crimes.lon
         }
-        // https://api.spotcrime.com/crimes.json?lat=" + place.lat + "&lon=" + place.lng + "&radius=0.02&key=heythisisforpublicspotcrime.comuse-forcommercial-or-research-use-call-877.410.1607-or-email-pyrrhus-at-spotcrime.com
-        var queryURL= "https://api.spotcrime.com/crimes.json?lat=33.775618&lon=-84.396285&radius=0.02&key=heythisisforpublicspotcrime.comuse-forcommercial-or-research-use-call-877.410.1607-or-email-pyrrhus-at-spotcrime.com";
-        
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(response.crimes[0]);
-            var crimeLocation = [
-                {
-                  lat: response.crimes.lat,
-                  lng: response.crimes.lon
-                }
-            ];
-        // test the above function before presentation to make sure we don't need to place a call to the function here
-
-        var response = [{
-            "type":"",
-            "date":""
-           }
-        ];
-      
-      // convert string to JSON
-      response = $.parseJSON(response);
-        
-        $("#crimeTableBody").append(
-            "<tr><td>" + response.crimes.type + "</td>" +
-            "<td>" + response.crimes.date + "</td></tr>"
-        );
+    ];
+    
+    var response = [{
+        type: response.crimes[0].type,
+        date: response.crimes[0].date
+    }
+];
+// test the above function before presentation to make sure we don't need to place a call to the function here
 
 
+$("#crimeTableBody").append(
+    "<tr><td></td>" +
+    "<td>" + response.crimes.type + "</td>" +
+    "<td>" + response.crimes.date + "</td></tr>"
+);
+
+})
